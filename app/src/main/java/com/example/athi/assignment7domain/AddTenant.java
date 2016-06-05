4a484a89e8df6720ab98a3b921054900ae0f3f65
Athi Sithembiso <asithembiso@gmail.com>
2016-06-05 14:16:35 +0200

    Added new Activities

---
+++ app/src/main/java/com/example/athi/assignment7domain/AddTenant.java
@@ -0,0 +1,13 @@
+package com.example.athi.assignment7domain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTenant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dd_tenant);
+    }
+}
---
+++ app/src/main/java/com/example/athi/assignment7domain/PreviewTenant.java
@@ -0,0 +1,13 @@
+package com.example.athi.assignment7domain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreviewTenant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_tenant);
+    }
+}
